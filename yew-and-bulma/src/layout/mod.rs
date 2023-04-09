92bab94ec02ccecf2c0e678f4517a1d29281b623
Filip Dutescu <filip@hucksy.dev>
2023-04-09 23:11:57 +0300

    feat(layout): add the section component
    Implement the [Bulma section element][bd] as a [Yew][yew] component.
    Document it and its properties and add an example showcasing how it's
    used. Update the module tree to include the section module and document
    the new additions to the crate.
    
    [bd]: https://bulma.io/documentation/layout/section/
    [yew]: https://yew.rs
    
    Fixes: #8
    Signed-off-by: Filip Dutescu <filip@hucksy.dev>

--- yew-and-bulma/src/layout/mod.rs
+++ yew-and-bulma/src/layout/mod.rs
@@ -19,3 +19,24 @@
 ///
 /// [bd]: https://bulma.io/documentation/layout/container/
 pub mod container;
+/// Provides utilities for creating [section elements][bd] in Yew.
+///
+/// Defines the necessary components to build, style and modify
+/// [Bulma section elements][bd] in Yew.
+///
+/// # Examples
+///
+/// ```rust
+/// use yew::prelude::*;
+/// use yew_and_bulma::layout::section::Section;
+///
+/// #[function_component(App)]
+/// fn app() -> Html {
+///     html! {
+///         <Section>{"This is some text in a section."}</Section>
+///     }
+/// }
+/// ```
+///
+/// [bd]: https://bulma.io/documentation/layout/section/
+pub mod section;
