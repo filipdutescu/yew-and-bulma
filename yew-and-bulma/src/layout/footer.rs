4b2faf25d6cb507de73771c9c2fa08f66b01a081
Filip Dutescu <filip@hucksy.dev>
2023-04-09 23:29:59 +0300

    feat(layout): add the footer component
    Implement the [Bulma footer element][bd] as a [Yew][yew] component.
    Document it and its properties and add an example showcasing how it's
    used. Update the module tree to include the footer module and document
    the new additions to the crate.
    
    [bd]: https://bulma.io/documentation/layout/footer/
    [yew]: https://yew.rs
    
    Fixes: #9
    Signed-off-by: Filip Dutescu <filip@hucksy.dev>

---
+++ yew-and-bulma/src/layout/footer.rs
@@ -0,0 +1,94 @@
+use yew::{function_component, html, Children, Html, Properties};
+use yew_and_bulma_macros::base_component_properties;
+
+use crate::utils::class::ClassBuilder;
+
+/// Defines the points from which a [footer element][bd] is not full width.
+///
+/// Defines the points from which a [Bulma footer element][bd] no longer has
+/// full width, but rather a fixed one. Those points are viewports from which
+/// the component is "active", concretely, space around the children is
+/// visible.
+///
+/// # Examples
+///
+/// ```rust
+/// use yew::prelude::*;
+/// use yew_and_bulma::layout::footer::footer;
+///
+/// #[function_component(App)]
+/// fn app() -> Html {
+///     html! {
+///         <Footer>
+///             {"This is some text in a footer."}
+///         </Footer>
+///     }
+/// }
+/// ```
+///
+/// [bd]: https://bulma.io/documentation/layout/footer/
+#[base_component_properties]
+#[derive(Properties, PartialEq)]
+pub struct FooterProperties {
+    /// The list of elements found inside the [footer element][bd].
+    ///
+    /// Defines the elements that will be found inside the
+    /// [Bulma footer element][bd] which will receive these properties.
+    ///
+    /// [bd]: https://bulma.io/documentation/layout/footer/
+    pub children: Children,
+}
+
+/// Yew implementation of the [Bulma footer element][bd].
+///
+/// Yew implementation of the footer element, based on the specification
+/// found in the [Bulma footer element documentation][bd].
+///
+/// # Examples
+///
+/// ```rust
+/// use yew::prelude::*;
+/// use yew_and_bulma::layout::footer::Footer;
+///
+/// #[function_component(App)]
+/// fn app() -> Html {
+///     html! {
+///         <Footer>
+///             {"This is some text in a footer."}
+///         </Footer>
+///     }
+/// }
+/// ```
+///
+/// [bd]: https://bulma.io/documentation/layout/footer/
+#[function_component(Footer)]
+pub fn footer(props: &FooterProperties) -> Html {
+    let class = ClassBuilder::default()
+        .with_custom_class("footer")
+        .with_custom_class(
+            &props
+                .class
+                .as_ref()
+                .map(|c| c.to_string())
+                .unwrap_or("".to_owned()),
+        )
+        .build();
+
+    html! {
+        <div id={props.id.clone()} {class}
+            onclick={props.onclick.clone()} onwheel={props.onwheel.clone()} onscroll={props.onscroll.clone()}
+            onmousedown={props.onmousedown.clone()} onmousemove={props.onmousemove.clone()} onmouseout={props.onmouseout.clone()} onmouseover={props.onmouseover.clone()} onmouseup={props.onmouseup.clone()}
+            ondrag={props.ondrag.clone()} ondragend={props.ondragend.clone()} ondragenter={props.ondragenter.clone()} ondragleave={props.ondragleave.clone()} ondragover={props.ondragover.clone()} ondragstart={props.ondragstart.clone()} ondrop={props.ondrop.clone()}
+            oncopy={props.oncopy.clone()} oncut={props.oncut.clone()} onpaste={props.onpaste.clone()}
+            onkeydown={props.onkeydown.clone()} onkeypress={props.onkeypress.clone()} onkeyup={props.onkeyup.clone()}
+            onblur={props.onblur.clone()} onchange={props.onchange.clone()} oncontextmenu={props.oncontextmenu.clone()} onfocus={props.onfocus.clone()} oninput={props.oninput.clone()} oninvalid={props.oninvalid.clone()} onreset={props.onreset.clone()} onselect={props.onselect.clone()} onsubmit={props.onsubmit.clone()}
+            onabort={props.onabort.clone()} oncanplay={props.oncanplay.clone()} oncanplaythrough={props.oncanplaythrough.clone()} oncuechange={props.oncuechange.clone()}
+            ondurationchange={props.ondurationchange.clone()} onemptied={props.onemptied.clone()} onended={props.onended.clone()} onerror={props.onerror.clone()}
+            onloadeddata={props.onloadeddata.clone()} onloadedmetadata={props.onloadedmetadata.clone()} onloadstart={props.onloadstart.clone()} onpause={props.onpause.clone()}
+            onplay={props.onplay.clone()} onplaying={props.onplaying.clone()} onprogress={props.onprogress.clone()} onratechange={props.onratechange.clone()}
+            onseeked={props.onseeked.clone()} onseeking={props.onseeking.clone()} onstalled={props.onstalled.clone()} onsuspend={props.onsuspend.clone()}
+            ontimeupdate={props.ontimeupdate.clone()} onvolumechange={props.onvolumechange.clone()} onwaiting={props.onwaiting.clone()}>
+            { for props.children.iter() }
+        </div>
+    }
+}
